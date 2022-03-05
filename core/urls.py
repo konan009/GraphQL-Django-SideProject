c4fb54530dc76e0038f5708f8491dd7cc4b5b3fa
Mel Ugaddan <ugaddanmeljohn@gmail.com>
2022-03-06 01:58:52 +0800

    Rename django applicaiton

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
-"""composeexample URL Configuration
+"""core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
-    path('', include('polls.urls'))
+    path('', include('books.urls'))
 ]
